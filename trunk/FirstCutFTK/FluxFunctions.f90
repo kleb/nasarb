decca5a6f2369b2b0e7ada132818f1f91f0b7992
cvs4bk <cvs4bk@15d20c99-a110-0410-8ff5-f949c1c7ce36>
2001-10-18 17:38:29 +0000

    improved status comments for tests macro to expand tests created state variable inTest
    git-svn-id: svn+ssh://rubyforge.org/var/svn/nasarb@19 15d20c99-a110-0410-8ff5-f949c1c7ce36

--- trunk/FirstCutFTK/FluxFunctions.f90
+++ trunk/FirstCutFTK/FluxFunctions.f90
@@ -18,4 +18,15 @@ contains
 
   end subroutine CentralFlux
 
+  subroutine RoeFlux (LeftState, RightState, InterfaceFlux)
+
+    real, intent(in)  :: LeftState
+    real, intent(in)  :: RightState
+
+    real, intent(out) :: InterfaceFlux
+
+    InterfaceFlux = 0.5*(LeftState+RightState)
+
+  end subroutine RoeFlux
+
 end module FluxFunctions
--- trunk/FirstCutFTK/FluxFunctionsTest.F90
+++ trunk/FirstCutFTK/FluxFunctionsTest.F90
@@ -8,10 +8,24 @@ subroutine TestFluxFunctions
   implicit none
 
   real :: Flux
+  real :: Right
+  real :: Left
 
   continue
 
-  call CentralFlux( 0.0, 1.0, Flux)
+  FTKSetup
+
+  Right = 0
+  Left  = 1
+
+  FTKTestSub CentralFlux( 0.0, 1.0, Flux )
+
+  IsFloatEqual(Flux, 0.5, 0.001)
+  IsFloatEqual(Flux, 0.5, 0.00000001)
+  IsEqual(Flux, 0.5)
+  IsEqual(Flux, 1.0)
+
+  FTKTestSub RoeFlux( 0.0, 1.0, Flux)
 
   IsFloatEqual(Flux, 0.5, 0.001)
   IsFloatEqual(Flux, 0.5, 0.00000001)
