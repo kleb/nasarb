d8d37201ea21b6202af0aea5764c83fa6489de05
cvs4bk <cvs4bk@15d20c99-a110-0410-8ff5-f949c1c7ce36>
2001-10-25 03:57:39 +0000

    added new and changed asserts
    git-svn-id: svn+ssh://rubyforge.org/var/svn/nasarb@41 15d20c99-a110-0410-8ff5-f949c1c7ce36

--- trunk/FirstCutFTK/FTK-mode.el
+++ trunk/FirstCutFTK/FTK-mode.el
@@ -10,10 +10,11 @@
     "beginTeardown"
     "endTeardown"
    )
-   '(("\\(IsFalse\\)"           1       'font-lock-function-name-face)
-     ("\\(IsTrue\\)"            1       'font-lock-function-name-face)
-     ("\\(IsEqual\\)"           1       'font-lock-function-name-face)
-     ("\\(IsFloatEqual\\)"      1       'font-lock-function-name-face))
+   '(("\\(IsFalse\\)"		1	'font-lock-function-name-face)
+     ("\\(IsTrue\\)"		1	'font-lock-function-name-face)
+     ("\\(IsEqualWithin\\)"	1	'font-lock-function-name-face)
+     ("\\(IsEqual\\)"		1	'font-lock-function-name-face)
+     ("\\(IsRealEqual\\)"	1	'font-lock-function-name-face))
    (list "\\.ftk\\'")
    nil
    "Generic mode for Fortran Test Kit files.")
