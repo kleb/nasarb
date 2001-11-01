37f834c386025bcea005a85c81a0eaf385a27d78
cvs4bk <cvs4bk@15d20c99-a110-0410-8ff5-f949c1c7ce36>
2001-11-01 03:18:57 +0000

    new FTK class
    git-svn-id: svn+ssh://rubyforge.org/var/svn/nasarb@58 15d20c99-a110-0410-8ff5-f949c1c7ce36

---
+++ trunk/FortranTK/src/TestSuite.rb
@@ -0,0 +1,105 @@
+class TestSuite < File
+
+ require 'Asserts'
+
+ def initialize suiteName
+
+  @suiteName = suiteName
+  super(suiteName+"TS.f90","w")
+  @tests, @setup, @teardown = [], [], []
+
+  puts <<-TOP
+! #{@suiteName}TS.f90 - a Fortran mobility test suite
+!
+! [dynamically generated from #{@suiteName}TS.ftk
+!  by #{File.basename $0} Ruby script #{Time.now}]
+
+module #{@suiteName}TestSuite
+
+ use #{@suiteName}
+
+ implicit none
+
+ private
+
+ public :: TS#{@suiteName}
+
+ logical :: noAssertFailed
+
+ integer :: numTests          = 0
+ integer :: numAsserts        = 0
+ integer :: numAssertsTested  = 0
+ integer :: numFailures       = 0
+
+  TOP
+ end
+
+ def addtoSetup
+  @setup.push($_) until gets=~/endSetup/
+ end
+
+ def addtoTeardown
+  @teardown.push($_) until gets=~/endTeardown/
+ end
+
+ def aTest testName
+  @tests.push(testName)
+  puts " subroutine Test#{testName}\n\n"
+
+  asserts = Asserts.new(testName,@suiteName)
+  until gets=~/endTest/
+   case $_
+    when /Is.*\(.*\)/
+     puts "Assertion Expanded Here"
+    else
+     puts $_
+   end
+  end
+
+  puts "\n  numTests = numTests + 1\n\n"
+  puts " end subroutine Test#{testName}\n\n"
+ end
+
+ def close
+  puts "\n subroutine Setup"
+  puts @setup
+  puts "  noAssertFailed = .true."
+  puts " end subroutine Setup\n\n"
+
+  puts "\n subroutine Teardown"
+  puts @teardown
+  puts " end subroutine Teardown\n\n"
+
+  puts <<-NEXTONE
+
+ subroutine TS#{@suiteName}( nTests, nAsserts, nAssertsTested, nFailures )
+
+  integer :: nTests
+  integer :: nAsserts
+  integer :: nAssertsTested
+  integer :: nFailures
+
+  continue
+  NEXTONE
+
+  @tests.each do |testName|
+   puts "\n  call Setup"
+   puts "  call Test#{testName}"
+   puts "  call Teardown"
+  end
+
+  puts <<-LASTONE
+
+  nTests          = numTests
+  nAsserts        = numAsserts
+  nAssertsTested  = numAssertsTested
+  nFailures       = numFailures
+
+ end subroutine TS#{@suiteName}
+
+end module #{@suiteName}TestSuite
+  LASTONE
+  super
+ end
+
+end
