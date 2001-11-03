b8e0dc31c2c60ae782ef88759ffa2c45ff9ec4f6
cvs4bk <cvs4bk@15d20c99-a110-0410-8ff5-f949c1c7ce36>
2001-11-03 03:09:47 +0000

    added some installation instructions
    git-svn-id: svn+ssh://rubyforge.org/var/svn/nasarb@63 15d20c99-a110-0410-8ff5-f949c1c7ce36

---
+++ trunk/FortranTK/etc/ftk-mode.el
@@ -0,0 +1,47 @@
+;; 'ftk-mode.el' - a derived major mode for editing Fortran Test Kit files
+;;
+;; $Id$
+;;
+;; INSTALLATION:
+;;
+;; Copy 'ftk-mode.el' to the system site-lisp directory, e.g.,
+;;
+;;   /usr/share/emacs/site-lisp
+;;
+;; or, if you do not have root permission, somewhere local
+;; and set the load-path search variable in your ~/.emacs file
+;; accordingly, e.g.,
+;;
+;;  (setq load-path (append load-path '("~/lisp")))
+;;
+;; Then, to automatically activate ftk-mode when visiting files,
+;; add the following lines to your ~/.emacs file:
+;;
+;;  (autoload 'ftk-mode "ftk-mode"
+;;     "Mode for editing Fortran Test Kit files.")
+;;  (setq auto-mode-alist
+;;     (cons '("\\TS.ftk$" . ftk-mode) auto-mode-alist))
+
+(define-derived-mode ftk-mode
+  f90-mode  "FortranTK"
+  "Major mode for Fortran Test Kit files (derived from F90 mode).
+  \\{ftk-mode-map}"
+ (font-lock-add-keywords
+  'ftk-mode
+  '(("\\<IsFalse\\>"		. font-lock-function-name-face)
+    ("\\<IsEqual\\>"		. font-lock-function-name-face)
+    ("\\<IsRealEqual\\>"	. font-lock-function-name-face)
+    ("\\<IsTrue\\>"		. font-lock-function-name-face)
+    ("\\<IsEqualWithin\\>"	. font-lock-function-name-face)
+    ("\\<beginTest\\>"		. font-lock-builtin-face)
+    ("\\<endTest\\>"		. font-lock-builtin-face)
+    ("\\<beginTeardown\\>"	. font-lock-builtin-face)
+    ("\\<endTeardown\\>"	. font-lock-builtin-face)
+    ("\\<beginSetup\\>"		. font-lock-builtin-face)
+    ("\\<endSetup\\>"		. font-lock-builtin-face))
+ )
+)
+
+(provide 'ftk-mode)
+
+;; end of 'ftk-mode.el'
