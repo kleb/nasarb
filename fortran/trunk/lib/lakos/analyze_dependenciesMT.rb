8b35d0d49a0cde008eda18be3f0190acd4e955ea
kleb <kleb@15d20c99-a110-0410-8ff5-f949c1c7ce36>
2007-08-02 12:06:40 +0000

    Initial import from FUN3D repository.
    git-svn-id: svn+ssh://rubyforge.org/var/svn/nasarb@295 15d20c99-a110-0410-8ff5-f949c1c7ce36

---
+++ fortran/trunk/lib/lakos/analyze_dependenciesMT.rb
@@ -0,0 +1,34 @@
+#!/usr/bin/env ruby
+#
+# Mobility Tests
+
+require 'test/unit'
+require 'analyze_dependencies'
+require 'FileUtils'
+
+class TestAnalyzeDependencies < Test::Unit::TestCase
+ def setup
+  Dir.mkdir 'AD_fixtures'
+  Dir.chdir 'AD_fixtures'
+  File.open('m.f90','w'){|f| f.puts "program m\nuse a\nuse b\nuse d\nuse f" }
+  File.open('a.f90','w'){|f| f.puts "module a" }
+  File.open('b.f90','w'){|f| f.puts "module b\nuse c\nuse d\nuse e" }
+  File.open('c.f90','w'){|f| f.puts "module c" }
+  File.open('d.f90','w'){|f| f.puts "module d" }
+  File.open('e.f90','w'){|f| f.puts "module e" }
+  File.open('f.f90','w'){|f| f.puts "module f\nuse c\nuse e" }
+  @da = DependencyAnalyzer.new( 'm.f90', ['.'] )
+  Dir.chdir '..'
+  File.open('main.dot','w'){ |f| f.puts @da.graph(@da.dd_deps) }
+ end
+ def test_statistics
+  assert_equal(  3, @da.levels.size )
+  assert_equal(  7, @da.components )
+  assert_equal( 18, @da.ccd )
+  assert_in_delta( 18/7.to_f,  @da.acd,  Float::EPSILON )
+  assert_in_delta( 18/17.to_f, @da.nccd, Float::EPSILON )
+ end
+ def teardown
+  FileUtils.rm_rf 'AD_fixtures'
+ end
+end
