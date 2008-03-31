e49601675e5829485bc3e9e020313712e138a11a
kleb <kleb@15d20c99-a110-0410-8ff5-f949c1c7ce36>
2008-03-31 02:08:09 +0000

    Thanks to rake multi, use Hash.delete instead of []=nil.
    git-svn-id: svn+ssh://rubyforge.org/var/svn/nasarb@408 15d20c99-a110-0410-8ff5-f949c1c7ce36

--- funit/trunk/test/test_compiler.rb
+++ funit/trunk/test/test_compiler.rb
@@ -6,7 +6,7 @@ class TestCompiler < Test::Unit::TestCase
   def test_no_environment_compiler_name
     begin
       orig_FC = ENV['FC']
-      ENV['FC'] = nil
+      ENV.delete 'FC'
       assert_raises(RuntimeError) {Funit::Compiler.new}
     ensure
       ENV['FC'] = orig_FC
